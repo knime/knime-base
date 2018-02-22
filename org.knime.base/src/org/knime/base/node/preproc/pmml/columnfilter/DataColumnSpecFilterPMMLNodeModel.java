kapoorlab/knime-core

AP-8665: added autoclosable supplier, deprecated and replaced method
* Added an autoclosable supplier using a ReentrantLock to guarantee thread-safety.
* Deprecated the getDocument method in XMLValue and added getDocumentSupplier.
* Changed callings of getDocument to getDocumentSupplier

AP-8665 (XML documents are not thread-safe)

org.knime.base/src/org/knime/base/node/preproc/pmml/columnfilter/DataColumnSpecFilterPMMLNodeModel.java
@@ -68,6 +68,7 @@ import org.knime.base.node.preproc.filter.column.DataColumnSpecFilterNodeModel;
 import org.knime.core.data.DataColumnSpec;
 import org.knime.core.data.DataTableSpec;
 import org.knime.core.data.container.ColumnRearranger;
+import org.knime.core.data.util.AutoclosableSupplier;
 import org.knime.core.node.BufferedDataTable;
 import org.knime.core.node.ExecutionContext;
 import org.knime.core.node.InvalidSettingsException;
@@ -199,8 +200,11 @@ public class DataColumnSpecFilterPMMLNodeModel extends DataColumnSpecFilterNodeM
         if (pmmlIn == null) {
             return new PMMLPortObject(createPMMLSpec(null, outSpec, res));
         } else {
-            Document doc = pmmlIn.getPMMLValue().getDocument();
-            PMMLDocument pmmldoc = PMMLDocument.Factory.parse(doc);
+            PMMLDocument pmmldoc = null;
+
+            try (AutoclosableSupplier<Document> supplier = pmmlIn.getPMMLValue().getDocumentSupplier()) {
+                pmmldoc = PMMLDocument.Factory.parse(supplier.getObject());
+            }
 
             // Inspect models to check if they use any excluded columns
             List<PMMLModelWrapper> models = PMMLModelWrapper.getModelListFromPMMLDocument(pmmldoc);

org.knime.base/src/org/knime/base/node/preproc/pmml/missingval/apply/MissingValueApplyNodeModel.java
@@ -8,6 +8,7 @@ import org.knime.base.node.preproc.pmml.missingval.MissingCellReplacingDataTable
 import org.knime.core.data.DataRow;
 import org.knime.core.data.DataTableSpec;
 import org.knime.core.data.container.DataContainer;
+import org.knime.core.data.util.AutoclosableSupplier;
 import org.knime.core.node.BufferedDataTable;
 import org.knime.core.node.CanceledExecutionException;
 import org.knime.core.node.ExecutionContext;
@@ -20,6 +21,7 @@ import org.knime.core.node.port.PortObject;
 import org.knime.core.node.port.PortObjectSpec;
 import org.knime.core.node.port.PortType;
 import org.knime.core.node.port.pmml.PMMLPortObject;
+import org.w3c.dom.Document;
 
 /**
  * This is the model implementation of CompiledModelReader.
@@ -50,8 +52,11 @@ public class MissingValueApplyNodeModel extends NodeModel {
         DataTableSpec inSpec = inTable.getDataTableSpec();
 
         PMMLPortObject pmmlIn = (PMMLPortObject)inData[PMML_PORT_IDX];
-        MissingCellReplacingDataTable mvTable = new MissingCellReplacingDataTable(inSpec,
-                        PMMLDocument.Factory.parse(pmmlIn.getPMMLValue().getDocument()));
+        MissingCellReplacingDataTable mvTable = null;
+
+        try (AutoclosableSupplier<Document> supplier = pmmlIn.getPMMLValue().getDocumentSupplier()) {
+            mvTable = new MissingCellReplacingDataTable(inSpec, PMMLDocument.Factory.parse(supplier.getObject()));
+        }
 
         // Calculate the statistics
         mvTable.init(inTable, exec.createSubExecutionContext(0.5));

knime/knime-base

org.knime.base/src/org/knime/base/node/preproc/pmml/columnfilter/DataColumnSpecFilterPMMLNodeModel.java
@@ -68,6 +68,7 @@ import org.knime.base.node.preproc.filter.column.DataColumnSpecFilterNodeModel;
 import org.knime.core.data.DataColumnSpec;
 import org.knime.core.data.DataTableSpec;
 import org.knime.core.data.container.ColumnRearranger;
+import org.knime.core.data.util.AutocloseableSupplier;
 import org.knime.core.node.BufferedDataTable;
 import org.knime.core.node.ExecutionContext;
 import org.knime.core.node.InvalidSettingsException;
@@ -199,8 +200,11 @@ public class DataColumnSpecFilterPMMLNodeModel extends DataColumnSpecFilterNodeM
         if (pmmlIn == null) {
             return new PMMLPortObject(createPMMLSpec(null, outSpec, res));
         } else {
-            Document doc = pmmlIn.getPMMLValue().getDocument();
-            PMMLDocument pmmldoc = PMMLDocument.Factory.parse(doc);
+            PMMLDocument pmmldoc = null;
+
+            try (AutocloseableSupplier<Document> supplier = pmmlIn.getPMMLValue().getDocumentSupplier()) {
+                pmmldoc = PMMLDocument.Factory.parse(supplier.get());
+            }
 
             // Inspect models to check if they use any excluded columns
             List<PMMLModelWrapper> models = PMMLModelWrapper.getModelListFromPMMLDocument(pmmldoc);

org.knime.base/src/org/knime/base/node/preproc/pmml/missingval/apply/MissingValueApplyNodeModel.java
@@ -8,6 +8,7 @@ import org.knime.base.node.preproc.pmml.missingval.MissingCellReplacingDataTable
 import org.knime.core.data.DataRow;
 import org.knime.core.data.DataTableSpec;
 import org.knime.core.data.container.DataContainer;
+import org.knime.core.data.util.AutocloseableSupplier;
 import org.knime.core.node.BufferedDataTable;
 import org.knime.core.node.CanceledExecutionException;
 import org.knime.core.node.ExecutionContext;
@@ -20,6 +21,7 @@ import org.knime.core.node.port.PortObject;
 import org.knime.core.node.port.PortObjectSpec;
 import org.knime.core.node.port.PortType;
 import org.knime.core.node.port.pmml.PMMLPortObject;
+import org.w3c.dom.Document;
 
 /**
  * This is the model implementation of CompiledModelReader.
@@ -50,8 +52,11 @@ public class MissingValueApplyNodeModel extends NodeModel {
         DataTableSpec inSpec = inTable.getDataTableSpec();
 
         PMMLPortObject pmmlIn = (PMMLPortObject)inData[PMML_PORT_IDX];
-        MissingCellReplacingDataTable mvTable = new MissingCellReplacingDataTable(inSpec,
-                        PMMLDocument.Factory.parse(pmmlIn.getPMMLValue().getDocument()));
+        MissingCellReplacingDataTable mvTable = null;
+
+        try (AutocloseableSupplier<Document> supplier = pmmlIn.getPMMLValue().getDocumentSupplier()) {
+            mvTable = new MissingCellReplacingDataTable(inSpec, PMMLDocument.Factory.parse(supplier.get()));
+        }
 
         // Calculate the statistics
         mvTable.init(inTable, exec.createSubExecutionContext(0.5));