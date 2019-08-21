b2389ae2b16154f3023618a8035c8101a0bc635f
Julian Bunzel <julian.bunzel@knime.com>
2019-08-21 16:05:57 +0200

    AP-12406: Updated SettingsModel
    AP-12406 (Make file reader dialog component prototype)

--- org.knime.filehandling.core/src/org/knime/filehandling/core/defaultnodesettings/SettingsModelFileChooserGen2.java
+++ org.knime.filehandling.core/src/org/knime/filehandling/core/defaultnodesettings/SettingsModelFileChooserGen2.java
@@ -52,27 +52,316 @@ import org.knime.core.node.InvalidSettingsException;
 import org.knime.core.node.NodeSettingsRO;
 import org.knime.core.node.NodeSettingsWO;
 import org.knime.core.node.NotConfigurableException;
+import org.knime.core.node.config.Config;
 import org.knime.core.node.defaultnodesettings.SettingsModel;
 import org.knime.core.node.defaultnodesettings.SettingsModelString;
 import org.knime.core.node.port.PortObjectSpec;
 
 /**
+ * SettingsModel for {@link DialogComponentFileChooserGen2}.
  *
- * @author bjoern
+ * @author Bjoern Lohrmann, KNIME GmbH, Berlin, Germany
+ * @author Julian Bunzel, KNIME GmbH, Berlin, Germany
  */
-public class SettingsModelFileChooserGen2 extends SettingsModel {
+public final class SettingsModelFileChooserGen2 extends SettingsModel {
 
-    private final SettingsModelString m_path = new SettingsModelString("path", "");
+    /** Configuration key for the option to select a specific file system. */
+    private static final String READ_FROM_FILE_SYSTEM = "read_from_file_system";
 
+    /** Configuration key to store the selected file system. */
+    private static final String SELECTED_FILE_SYSTEM = "selected_file_system";
+
+    /** Configuration key to store the path of the selected file or folder. */
+    private static final String FILE_OR_FOLDER_PATH = "path_of_file_or_folder";
+
+    /** Configuration key for the option to search sub-folders. */
+    private static final String SEARCH_SUB_FOLDERS = "search_subfolders";
+
+    /** Configuration key for the option to filter files in selected folder. */
+    private static final String FILTER_FILES_IN_FOLDER = "filter_files_in_folder";
+
+    /** Configuration key to store the filter mode. */
+    private static final String FILTER_MODE = "filter_mode";
+
+    /** Configuration key to store the filter expression. */
+    private static final String FILTER_EXPRESSION = "filter_expression";
+
+    /** Configuration key to store the setting whether the filter expression is case sensitive or not. */
+    private static final String FILTER_CASE_SENSITIVITY = "filter_case_sensitivity";
+
+    /** The name of the configuration object. */
     private final String m_configName;
 
+    /** True, if file/folder should be read from a file system other than local. */
+    private boolean m_readFromFileSystem = false;
+
+    /** The name of the selected file system. */
+    private String m_fileSystemFlowVarName = "Local File System";
+
+    /** Path of selected file or folder. */
+    private String m_pathOfFileOrFolder;
+
+    /** True, if sub-folders should be included. */
+    private boolean m_searchSubfolder;
+
+    /** True, if files should be filtered. */
+    private boolean m_filterFiles;
+
+    /** Mode used to filter files (e.g. regex or wildcard). */
+    private String m_filterMode;
+
+    /** The expression used to filter files in the selected folder/directory. */
+    private String m_filterExpression;
+
+    /** True, if expression to filter should work regardless the case of the filenames. */
+    private boolean m_filterCaseSensitivity;
+
+    // TODO For what again?
+    private final SettingsModelString m_path = new SettingsModelString("path", "");
+
+    /**
+     * Creates a new instance of {@link SettingsModelFileChooserGen2} with default settings.
+     *
+     * @param configName the name of the config.
+     */
     public SettingsModelFileChooserGen2(final String configName) {
+        this(configName, false, "", "", false, false, "wildcard", "*", false);
+    }
+
+    /**
+     * Creates a new instance of {@link SettingsModelFileChooserGen2}.
+     *
+     * @param configName the name of the configuration object.
+     * @param readFromFileSystem true, if file/folder should be read from a file system other than local
+     * @param fileSystemName the name of the selected file system
+     * @param pathOfFileOrFolder the path of the selected file or folder
+     * @param searchSubfolder true, if sub-folder should be included
+     * @param filterFiles true, if files should be filtered
+     * @param filterMode mode to filter files in case a multiple files should be read
+     * @param filterExpression the expression used to filter files
+     * @param caseSensitivity true, if expression should be sensitive to case
+     */
+    public SettingsModelFileChooserGen2(final String configName, final boolean readFromFileSystem,
+        final String fileSystemName, final String pathOfFileOrFolder, final boolean searchSubfolder,
+        final boolean filterFiles, final String filterMode, final String filterExpression,
+        final boolean caseSensitivity) {
         if ((configName == null) || "".equals(configName)) {
             throw new IllegalArgumentException("The configName must be a " + "non-empty string");
         }
         m_configName = configName;
+        m_readFromFileSystem = readFromFileSystem;
+        m_fileSystemFlowVarName = fileSystemName;
+        m_pathOfFileOrFolder = pathOfFileOrFolder;
+        m_searchSubfolder = searchSubfolder;
+        m_filterFiles = filterFiles;
+        m_filterMode = filterMode;
+        m_filterExpression = filterExpression;
+        m_filterCaseSensitivity = caseSensitivity;
     }
 
+    /**
+     * Sets a new value for the option to read from a specific file system other than the local one.
+     *
+     * @param newValue Set true, if file/folder should be read from a specific file system other than the local one
+     */
+    public void setReadFromFileSystem(final boolean newValue) {
+        boolean sameValue = (m_readFromFileSystem == newValue);
+        m_readFromFileSystem = newValue;
+        if (!sameValue) {
+            notifyChangeListeners();
+        }
+    }
+
+    /**
+     * Sets the name of the file system to select the file/folder from.
+     *
+     * @param newValue the name of the file system to select the file/folder from
+     */
+    public void setFileSystemName(final String newValue) {
+        boolean sameValue;
+
+        if (newValue == null) {
+            sameValue = (m_fileSystemFlowVarName == null);
+        } else {
+            sameValue = newValue.equals(m_fileSystemFlowVarName);
+        }
+        m_fileSystemFlowVarName = newValue;
+
+        if (!sameValue) {
+            notifyChangeListeners();
+        }
+    }
+
+    /**
+     * Sets the path of the file/folder.
+     *
+     * @param newValue The path of the file/folder
+     */
+    public void setPathOfFileOrFolder(final String newValue) {
+        boolean sameValue;
+
+        if (newValue == null) {
+            sameValue = (m_pathOfFileOrFolder == null);
+        } else {
+            sameValue = newValue.equals(m_pathOfFileOrFolder);
+        }
+        m_pathOfFileOrFolder = newValue;
+
+        if (!sameValue) {
+            notifyChangeListeners();
+        }
+    }
+
+    /**
+     * Sets a new value for the option to search sub folder for files.
+     *
+     * @param newValue Set true, if sub folder should be searched for files
+     */
+    public void setSearchSubfolder(final boolean newValue) {
+        boolean sameValue = (m_searchSubfolder == newValue);
+        m_searchSubfolder = newValue;
+        if (!sameValue) {
+            notifyChangeListeners();
+        }
+    }
+
+    /**
+     * Sets a new value for the option to filter files in a selected folder.
+     *
+     * @param newValue Set true, if files in a selected folder should be filtered
+     */
+    public void setFilterFiles(final boolean newValue) {
+        boolean sameValue = (m_filterFiles == newValue);
+        m_filterFiles = newValue;
+        if (!sameValue) {
+            notifyChangeListeners();
+        }
+    }
+
+    /**
+     * Sets the filter mode (e.g. wildcard or regular expression) to filter files in a selected folder.
+     *
+     * @param newValue The filter mode to filter files in a selected folder.
+     */
+    public void setFilterMode(final String newValue) {
+        boolean sameValue;
+
+        if (newValue == null) {
+            sameValue = (m_filterMode == null);
+        } else {
+            sameValue = newValue.equals(m_filterMode);
+        }
+        m_filterMode = newValue;
+
+        if (!sameValue) {
+            notifyChangeListeners();
+        }
+    }
+
+    /**
+     * Sets the filter expression.
+     *
+     * @param newValue The filter expression
+     */
+    public void setFilterExpression(final String newValue) {
+        boolean sameValue;
+
+        if (newValue == null) {
+            sameValue = (m_filterExpression == null);
+        } else {
+            sameValue = newValue.equals(m_filterExpression);
+        }
+        m_filterExpression = newValue;
+
+        if (!sameValue) {
+            notifyChangeListeners();
+        }
+    }
+
+    /**
+     * Sets the new value for the case sensitivity option.
+     *
+     * @param newValue New value for the case sensitivity option
+     */
+    public void setCaseSensitivity(final boolean newValue) {
+        boolean sameValue = (m_filterCaseSensitivity == newValue);
+        m_filterCaseSensitivity = newValue;
+        if (!sameValue) {
+            notifyChangeListeners();
+        }
+    }
+
+    /**
+     * Returns true, if file/folder should be read from a file system other than the local one.
+     *
+     * @return True, if file/folder should be read from a file system other than the local one
+     */
+    public boolean getReadFromFileSystem() {
+        return m_readFromFileSystem;
+    }
+
+    /**
+     * Returns the name of the selected file system.
+     *
+     * @return The name of the selected file system
+     */
+    public String getFileSystemName() {
+        return m_fileSystemFlowVarName;
+    }
+
+    /**
+     * Returns the path of the selected file or folder.
+     *
+     * @return The path of the selected file or folder
+     */
+    public String getPathOfFileOrFolder() {
+        return m_pathOfFileOrFolder;
+    }
+
+    /**
+     * Returns true, if sub folders should be included while searching files.
+     *
+     * @return True, if sub folders should be included while searching files
+     */
+    public boolean getSearchSubfolder() {
+        return m_searchSubfolder;
+    }
+
+    /**
+     * Returns true, if files should be filtered.
+     *
+     * @return True, if files should be filtered.
+     */
+    public boolean getFilterFiles() {
+        return m_filterFiles;
+    }
+
+    /**
+     * Returns the mode to filter files.
+     *
+     * @return The mode to filter files
+     */
+    public String getFilterMode() {
+        return m_filterMode;
+    }
+
+    /**
+     * Returns the expression used to filter files.
+     *
+     * @return The expression used to filter files
+     */
+    public String getFilterExpression() {
+        return m_filterExpression;
+    }
+
+    /**
+     * Returns true, if case sensitivity option is selected.
+     *
+     * @return True, if case sensitivity option is selected
+     */
+    public boolean getCaseSensitivity() {
+        return m_filterCaseSensitivity;
+    }
 
     /**
      * @return the path
@@ -81,80 +370,85 @@ public class SettingsModelFileChooserGen2 extends SettingsModel {
         return m_path;
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @SuppressWarnings("unchecked")
     @Override
     protected SettingsModelFileChooserGen2 createClone() {
         return new SettingsModelFileChooserGen2(m_configName);
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
     protected String getModelTypeID() {
         return "SMID_FileChooserGen2";
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
     protected String getConfigName() {
         return m_configName;
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
     protected void loadSettingsForDialog(final NodeSettingsRO settings, final PortObjectSpec[] specs)
         throws NotConfigurableException {
-        // TODO Auto-generated method stub
-
+        final Config config;
+        try {
+            config = settings.getConfig(m_configName);
+            setReadFromFileSystem(config.getBoolean(READ_FROM_FILE_SYSTEM, m_readFromFileSystem));
+            setFileSystemName(config.getString(SELECTED_FILE_SYSTEM, m_fileSystemFlowVarName));
+            setPathOfFileOrFolder(config.getString(FILE_OR_FOLDER_PATH, m_pathOfFileOrFolder));
+            setSearchSubfolder(config.getBoolean(SEARCH_SUB_FOLDERS, m_searchSubfolder));
+            setFilterFiles(config.getBoolean(FILTER_FILES_IN_FOLDER, m_filterFiles));
+            setFilterMode(config.getString(FILTER_MODE, m_filterMode));
+            setFilterExpression(config.getString(FILTER_EXPRESSION, m_filterExpression));
+            setCaseSensitivity(config.getBoolean(FILTER_CASE_SENSITIVITY, m_filterCaseSensitivity));
+        } catch (final InvalidSettingsException ise) {
+            throw new NotConfigurableException(ise.getMessage());
+        }
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
     protected void saveSettingsForDialog(final NodeSettingsWO settings) throws InvalidSettingsException {
-        // TODO Auto-generated method stub
-
+        saveSettingsForModel(settings);
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
     protected void validateSettingsForModel(final NodeSettingsRO settings) throws InvalidSettingsException {
-        // TODO Auto-generated method stub
-
+        final Config config = settings.getConfig(m_configName);
+        config.getBoolean(READ_FROM_FILE_SYSTEM);
+        config.getString(SELECTED_FILE_SYSTEM);
+        config.getString(FILE_OR_FOLDER_PATH);
+        config.getBoolean(SEARCH_SUB_FOLDERS);
+        config.getBoolean(FILTER_FILES_IN_FOLDER);
+        config.getString(FILTER_MODE);
+        config.getString(FILTER_EXPRESSION);
+        config.getBoolean(FILTER_CASE_SENSITIVITY);
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
     protected void loadSettingsForModel(final NodeSettingsRO settings) throws InvalidSettingsException {
-        // TODO Auto-generated method stub
-
+        final Config config = settings.getConfig(m_configName);
+        setReadFromFileSystem(config.getBoolean(READ_FROM_FILE_SYSTEM));
+        setFileSystemName(config.getString(SELECTED_FILE_SYSTEM));
+        setPathOfFileOrFolder(config.getString(FILE_OR_FOLDER_PATH));
+        setSearchSubfolder(config.getBoolean(SEARCH_SUB_FOLDERS));
+        setFilterFiles(config.getBoolean(FILTER_FILES_IN_FOLDER));
+        setFilterMode(config.getString(FILTER_MODE));
+        setFilterExpression(config.getString(FILTER_EXPRESSION));
+        setCaseSensitivity(config.getBoolean(FILTER_CASE_SENSITIVITY));
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
     protected void saveSettingsForModel(final NodeSettingsWO settings) {
-        // TODO Auto-generated method stub
-
+        final Config config = settings.addConfig(m_configName);
+        config.addBoolean(READ_FROM_FILE_SYSTEM, getReadFromFileSystem());
+        config.addString(SELECTED_FILE_SYSTEM, getFileSystemName());
+        config.addString(FILE_OR_FOLDER_PATH, getPathOfFileOrFolder());
+        config.addBoolean(SEARCH_SUB_FOLDERS, getSearchSubfolder());
+        config.addBoolean(FILTER_FILES_IN_FOLDER, getFilterFiles());
+        config.addString(FILTER_MODE, getFilterMode());
+        config.addString(FILTER_EXPRESSION, getFilterExpression());
+        config.addBoolean(FILTER_CASE_SENSITIVITY, getCaseSensitivity());
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
     public String toString() {
         return getClass().getSimpleName() + " ('" + m_configName + "')";
