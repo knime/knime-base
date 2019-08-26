f816796490f7c8c4bf731aa2b57b15945160b0fd
Benjamin Wilhelm <benjamin.wilhelm@uni-konstanz.de>
2019-08-26 16:36:19 +0200

    AP-10030: Change output of correlation node
    The output is now a table with the correlation value, p value and
    degrees of freedom. Previously it was a matrix with the correlation
    values.

--- org.knime.base/src/org/knime/base/node/preproc/correlation/compute2/CorrelationCompute2NodeModel.java
+++ org.knime.base/src/org/knime/base/node/preproc/correlation/compute2/CorrelationCompute2NodeModel.java
@@ -49,14 +49,27 @@ package org.knime.base.node.preproc.correlation.compute2;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Collections;
 
+import org.knime.base.node.preproc.correlation.compute2.CorrelationComputer2.CorrelationResult;
 import org.knime.base.node.preproc.correlation.pmcc.PMCCPortObjectAndSpec;
 import org.knime.base.util.HalfDoubleMatrix;
+import org.knime.base.util.HalfIntMatrix;
+import org.knime.core.data.DataColumnProperties;
+import org.knime.core.data.DataColumnSpec;
+import org.knime.core.data.DataColumnSpecCreator;
 import org.knime.core.data.DataTable;
 import org.knime.core.data.DataTableSpec;
 import org.knime.core.data.DoubleValue;
 import org.knime.core.data.NominalValue;
+import org.knime.core.data.RowKey;
 import org.knime.core.data.container.ColumnRearranger;
+import org.knime.core.data.def.DefaultRow;
+import org.knime.core.data.def.DoubleCell;
+import org.knime.core.data.def.IntCell;
+import org.knime.core.data.renderer.DataValueRenderer;
+import org.knime.core.data.renderer.DoubleValueRenderer.FullPrecisionRendererFactory;
+import org.knime.core.node.BufferedDataContainer;
 import org.knime.core.node.BufferedDataTable;
 import org.knime.core.node.BufferedDataTableHolder;
 import org.knime.core.node.CanceledExecutionException;
@@ -75,11 +88,49 @@ import org.knime.core.node.port.PortType;
 import org.knime.core.node.util.filter.NameFilterConfiguration.FilterResult;
 
 /**
- * @author wiswedel, University of Konstanz
+ * @author Bernd Wiswedel, University of Konstanz
+ * @author Benjamin Wilhelm, KNIME GmbH, Konstanz, Germany
  */
 final class CorrelationCompute2NodeModel extends NodeModel implements BufferedDataTableHolder {
 
+    private static final String CORRELATION_VALUE_COL_NAME = "Correlation value";
+
+    private static final String P_VALUE_COL_NAME = "p value";
+
+    private static final String DEGREES_OF_FREEDOM_COL_NAME = "Degrees of freedom";
+
+    /** Full precision renderer for double values */
+    private static final String FULL_PRECISION_RENDERER = new FullPrecisionRendererFactory().getDescription();
+
+    /** Progress of the first step */
+    private static final double PROG_STEP1 = 0.48;
+
+    /** Progress of the second step */
+    private static final double PROG_STEP2 = 0.48;
+
+    /** Progress of the last step */
+    private static final double PROG_FINISH = 1 - PROG_STEP1 - PROG_STEP2;
+
     private static final NodeLogger LOGGER = NodeLogger.getLogger(CorrelationCompute2NodeModel.class);
+
+    /**
+     * Factory method to instantiate a default settings object, used in constructor and in dialog.
+     *
+     * @return A new default settings object.
+     */
+    @SuppressWarnings("unchecked")
+    static SettingsModelColumnFilter2 createColumnFilterModel() {
+        return new SettingsModelColumnFilter2("include-list", DoubleValue.class, NominalValue.class);
+    }
+
+    /**
+     * Factory method to create the bounded range model for the possible values count.
+     *
+     * @return A new model.
+     */
+    static SettingsModelIntegerBounded createNewPossValueCounterModel() {
+        return new SettingsModelIntegerBounded("possibleValuesCount", 50, 2, Integer.MAX_VALUE);
+    }
 
     private SettingsModelColumnFilter2 m_columnFilterModel;
 
@@ -97,33 +148,80 @@ final class CorrelationCompute2NodeModel extends NodeModel implements BufferedDa
     }
 
     @Override
+    protected PortObjectSpec[] configure(final PortObjectSpec[] inSpecs) throws InvalidSettingsException {
+        DataTableSpec in = (DataTableSpec)inSpecs[0];
+        if (!in.containsCompatibleType(DoubleValue.class) && !in.containsCompatibleType(NominalValue.class)) {
+            throw new InvalidSettingsException("No double or nominal compatible columns in input");
+        }
+        final String[] includesNames;
+        if (m_columnFilterModel == null) {
+            m_columnFilterModel = createColumnFilterModel();
+            // auto-configure, no previous configuration
+            m_columnFilterModel.loadDefaults(in);
+            includesNames = m_columnFilterModel.applyTo(in).getIncludes();
+            setWarningMessage(
+                "Auto configuration: Using all suitable " + "columns (in total " + includesNames.length + ")");
+        } else {
+            FilterResult applyTo = m_columnFilterModel.applyTo(in);
+            includesNames = applyTo.getIncludes();
+        }
+        if (includesNames.length == 0) {
+            throw new InvalidSettingsException("No columns selected");
+        }
+        return new PortObjectSpec[]{createOutputTableSpec(), new PMCCPortObjectAndSpec(includesNames)};
+    }
+
+    private static DataTableSpec createOutputTableSpec() {
+        final DataColumnSpec corrColSpec =
+            new DataColumnSpecCreator(CORRELATION_VALUE_COL_NAME, DoubleCell.TYPE).createSpec();
+        final DataColumnSpecCreator pValColSpecCreator = new DataColumnSpecCreator(P_VALUE_COL_NAME, DoubleCell.TYPE);
+        // Set the full precision renderer for the p value column
+        pValColSpecCreator.setProperties(new DataColumnProperties(
+            Collections.singletonMap(DataValueRenderer.PROPERTY_PREFERRED_RENDERER, FULL_PRECISION_RENDERER)));
+        final DataColumnSpec pValColSpec = pValColSpecCreator.createSpec();
+        final DataColumnSpec dofColSpec =
+            new DataColumnSpecCreator(DEGREES_OF_FREEDOM_COL_NAME, DoubleCell.TYPE).createSpec();
+        return new DataTableSpec(corrColSpec, pValColSpec, dofColSpec);
+    }
+
+    @Override
     protected PortObject[] execute(final PortObject[] inData, final ExecutionContext exec) throws Exception {
         final BufferedDataTable in = (BufferedDataTable)inData[0];
         final DataTableSpec inSpec = in.getDataTableSpec();
+
+        // Filter selected columns
         ColumnRearranger filteredTableRearranger = new ColumnRearranger(inSpec);
         String[] includeNames = m_columnFilterModel.applyTo(inSpec).getIncludes();
         filteredTableRearranger.keepOnly(includeNames);
         final BufferedDataTable filteredTable =
             exec.createColumnRearrangeTable(in, filteredTableRearranger, exec.createSilentSubExecutionContext(0.0));
         final DataTableSpec filteredTableSpec = filteredTable.getDataTableSpec();
-        double progStep1 = 0.48;
-        double progStep2 = 0.48;
-        double progFinish = 1.0 - progStep1 - progStep2;
+
+        // Create the correlation computer
         CorrelationComputer2 calculator =
             new CorrelationComputer2(filteredTableSpec, m_maxPossValueCountModel.getIntValue());
+
+        // Calculate statistics on the table
         exec.setMessage("Calculating table statistics");
-        ExecutionContext execStep1 = exec.createSubExecutionContext(progStep1);
+        ExecutionContext execStep1 = exec.createSubExecutionContext(PROG_STEP1);
         calculator.calculateStatistics(filteredTable, execStep1);
         execStep1.setProgress(1.0);
+
+        // Calculate the correlation
         exec.setMessage("Calculating correlation values");
-        ExecutionMonitor execStep2 = exec.createSubExecutionContext(progStep2);
-        HalfDoubleMatrix correlationMatrix = calculator.calculateOutput(filteredTable, execStep2);
+        ExecutionMonitor execStep2 = exec.createSubExecutionContext(PROG_STEP2);
+        CorrelationResult correlationResult = calculator.calculateOutput(filteredTable, execStep2);
         execStep2.setProgress(1.0);
+
+        // Create the output
         exec.setMessage("Assembling output");
-        ExecutionContext execFinish = exec.createSubExecutionContext(progFinish);
-        PMCCPortObjectAndSpec pmccModel = new PMCCPortObjectAndSpec(includeNames, correlationMatrix);
-        BufferedDataTable out = pmccModel.createCorrelationMatrix(execFinish);
+        ExecutionContext execFinish = exec.createSubExecutionContext(PROG_FINISH);
+        PMCCPortObjectAndSpec pmccModel =
+            new PMCCPortObjectAndSpec(includeNames, correlationResult.getCorrelationMatrix());
+        BufferedDataTable out = createOutputTable(correlationResult, includeNames, execFinish);
         m_correlationTable = out;
+
+        // Warning handling
         String missValueString = calculator.getNumericMissingValueWarning(4);
         StringBuilder warning = null;
         if (missValueString != null) {
@@ -143,35 +241,46 @@ final class CorrelationCompute2NodeModel extends NodeModel implements BufferedDa
         if (warning != null) {
             setWarningMessage(warning.toString());
         }
-        return new PortObject[]{out, pmccModel};
 
+        return new PortObject[]{out, pmccModel};
+    }
+
+    private static BufferedDataTable createOutputTable(final CorrelationResult correlationResult,
+        final String[] includeNames, final ExecutionContext exec) throws CanceledExecutionException {
+        final DataTableSpec outSpec = createOutputTableSpec();
+        final BufferedDataContainer dataContainer = exec.createDataContainer(outSpec);
+
+        final HalfDoubleMatrix corrMatrix = correlationResult.getCorrelationMatrix();
+        final HalfDoubleMatrix pValMatrix = correlationResult.getpValMatrix();
+        final HalfIntMatrix dofMatrix = correlationResult.getDegreesOfFreedomMatrix();
+
+        // Fill the table
+        int numInc = includeNames.length;
+        int rowIndex = 0;
+        final double rowCount = numInc * (numInc - 1) / 2;
+        for (int i = 0; i < numInc; i++) {
+            for (int j = i + 1; j < numInc; j++) {
+                final DoubleCell corr = new DoubleCell(corrMatrix.get(i, j));
+                final DoubleCell pVal = new DoubleCell(pValMatrix.get(i, j));
+                final IntCell dof = new IntCell(dofMatrix.get(i, j));
+                final RowKey rowKey = new RowKey(getRowKey(includeNames[i], includeNames[j]));
+                final DefaultRow row = new DefaultRow(rowKey, corr, pVal, dof);
+                exec.checkCanceled();
+                dataContainer.addRowToTable(row);
+                exec.setProgress(++rowIndex / rowCount);
+            }
+        }
+        dataContainer.close();
+        return dataContainer.getTable();
+    }
+
+    private static String getRowKey(final String columnNameA, final String columnNameB) {
+        return columnNameA + "_" + columnNameB;
     }
 
     @Override
     protected void reset() {
-    }
-
-    @Override
-    protected PortObjectSpec[] configure(final PortObjectSpec[] inSpecs) throws InvalidSettingsException {
-        DataTableSpec in = (DataTableSpec)inSpecs[0];
-        if (!in.containsCompatibleType(DoubleValue.class) && !in.containsCompatibleType(NominalValue.class)) {
-            throw new InvalidSettingsException("No double or nominal compatible columns in input");
-        }
-        final String[] includes;
-        if (m_columnFilterModel == null) {
-            m_columnFilterModel = createColumnFilterModel();
-            // auto-configure, no previous configuration
-            m_columnFilterModel.loadDefaults(in);
-            includes = m_columnFilterModel.applyTo(in).getIncludes();
-            setWarningMessage("Auto configuration: Using all suitable " + "columns (in total " + includes.length + ")");
-        } else {
-            FilterResult applyTo = m_columnFilterModel.applyTo(in);
-            includes = applyTo.getIncludes();
-        }
-        if (includes.length == 0) {
-            throw new InvalidSettingsException("No columns selected");
-        }
-        return new PortObjectSpec[]{PMCCPortObjectAndSpec.createOutSpec(includes), new PMCCPortObjectAndSpec(includes)};
+        // nothing to do
     }
 
     @Override
@@ -209,30 +318,13 @@ final class CorrelationCompute2NodeModel extends NodeModel implements BufferedDa
     @Override
     protected void loadInternals(final File nodeInternDir, final ExecutionMonitor exec)
         throws IOException, CanceledExecutionException {
+        // nothing to do
     }
 
     @Override
     protected void saveInternals(final File nodeInternDir, final ExecutionMonitor exec)
         throws IOException, CanceledExecutionException {
-    }
-
-    /**
-     * Factory method to instantiate a default settings object, used in constructor and in dialog.
-     *
-     * @return A new default settings object.
-     */
-    @SuppressWarnings("unchecked")
-    static SettingsModelColumnFilter2 createColumnFilterModel() {
-        return new SettingsModelColumnFilter2("include-list", DoubleValue.class, NominalValue.class);
-    }
-
-    /**
-     * Factory method to create the bounded range model for the possible values count.
-     *
-     * @return A new model.
-     */
-    static SettingsModelIntegerBounded createNewPossValueCounterModel() {
-        return new SettingsModelIntegerBounded("possibleValuesCount", 50, 2, Integer.MAX_VALUE);
+        // nothing to do
     }
 
     @Override
@@ -244,5 +336,4 @@ final class CorrelationCompute2NodeModel extends NodeModel implements BufferedDa
     public void setInternalTables(final BufferedDataTable[] tables) {
         m_correlationTable = tables[0];
     }
-
 }
