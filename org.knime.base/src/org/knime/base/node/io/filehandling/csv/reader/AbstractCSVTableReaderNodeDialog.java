1587db94cc6412cfa15d06620caaee265dc829a5
Bernd Wiswedel <Bernd.Wiswedel@knime.com>
2020-07-10 15:08:55 +0200

    AP-14748: CSV Reader: #onClose in EDT thread
    AP-14748: Closing (some) node configuration dialogs may cause freeze on MacOS

--- org.knime.base/src/org/knime/base/node/io/filehandling/csv/reader/AbstractCSVTableReaderNodeDialog.java
+++ org.knime.base/src/org/knime/base/node/io/filehandling/csv/reader/AbstractCSVTableReaderNodeDialog.java
@@ -92,6 +92,7 @@ import org.knime.core.node.NodeSettingsWO;
 import org.knime.core.node.NotConfigurableException;
 import org.knime.core.node.port.PortObjectSpec;
 import org.knime.core.node.util.SharedIcons;
+import org.knime.core.node.util.ViewUtils;
 import org.knime.filehandling.core.node.table.reader.MultiTableReader;
 import org.knime.filehandling.core.node.table.reader.config.DefaultMultiTableReadConfig;
 import org.knime.filehandling.core.node.table.reader.config.DefaultTableReadConfig;
@@ -1021,8 +1022,10 @@ public abstract class AbstractCSVTableReaderNodeDialog extends NodeDialogPane {
 
     @Override
     public void onClose() {
-        cancelFormatAutoDetection();
-        m_tableReaderPreview.onClose();
+        ViewUtils.invokeAndWaitInEDT(() -> {
+            cancelFormatAutoDetection();
+            m_tableReaderPreview.onClose();
+        });
         super.onClose();
     }
 }
