f0818ab9fb69f54d19d3b54d73a02a56fd529cfa
Lars Schweikardt <lars.schweikardt@knime.com>
2020-10-07 15:24:05 +0200

    AP-15186: Added CheckedExBiFunction
    Added a CheckedExceptionBiFunction to the filehandling.core.util to make
    use of for instance in the copy move node.
    
    AP-15186 (Avoid downloading data in Copy/Move Files/Folders)

---
+++ org.knime.filehandling.core/src/org/knime/filehandling/core/util/CheckedExceptionBiFunction.java
@@ -0,0 +1,77 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright by KNIME AG, Zurich, Switzerland
+ *  Website: http://www.knime.com; Email: contact@knime.com
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME AG herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ *
+ * History
+ *   7 Oct 2020 (lars.schweikardt): created
+ */
+package org.knime.filehandling.core.util;
+
+import java.util.function.BiFunction;
+
+/**
+ * A {@link BiFunction} that throws a checked exception.
+ *
+ * @author Lars Schweikardt, KNIME GmbH, Konstanz, Germany
+ * @param <T> The first input type of the function
+ * @param <S> The second input type of the function
+ * @param <R> The return type of the function
+ * @param <E> The exception thrown by the function
+ * @noreference non-public API
+ * @noimplement non-public API
+ */
+@FunctionalInterface
+public interface CheckedExceptionBiFunction<T, S, R, E extends Exception> {
+
+    /**
+     * Apply the function to the input t and s and returns an Object of type <R>. Might throw an exception <E>
+     *
+     * @param t Input of type <T>
+     * @param s Input of type <S>
+     * @return object of type <R>
+     * @throws E <E>
+     */
+    R apply(T t, S s) throws E;
+
+}
