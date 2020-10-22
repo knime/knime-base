1d20eef8268f38d4d51c6ad4606ec4b6b019b94d
Ayaz Ali Qureshi <ayaz.qureshi@knime.com>
2020-10-22 13:26:17 +0200

    AP-15265: Implement configure of the Binary Objects to Files
    - Add autoGuess and validate functionalities in the NodeModel
    
    AP-15265 Implement configure of the Binary Objects to Files (Labs)

--- org.knime.filehandling.utility.nodes/src/org/knime/filehandling/utility/nodes/binaryobjects/writer/BinaryObjectsToFilesNodeModel.java
+++ org.knime.filehandling.utility.nodes/src/org/knime/filehandling/utility/nodes/binaryobjects/writer/BinaryObjectsToFilesNodeModel.java
@@ -50,7 +50,13 @@ package org.knime.filehandling.utility.nodes.binaryobjects.writer;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.EnumSet;
 
+import org.knime.core.data.DataColumnSpec;
+import org.knime.core.data.DataColumnSpecCreator;
+import org.knime.core.data.DataTableSpec;
+import org.knime.core.data.blob.BinaryObjectDataValue;
+import org.knime.core.data.container.ColumnRearranger;
 import org.knime.core.node.CanceledExecutionException;
 import org.knime.core.node.ExecutionMonitor;
 import org.knime.core.node.InvalidSettingsException;
@@ -58,6 +64,15 @@ import org.knime.core.node.NodeModel;
 import org.knime.core.node.NodeSettingsRO;
 import org.knime.core.node.NodeSettingsWO;
 import org.knime.core.node.context.ports.PortsConfiguration;
+import org.knime.core.node.defaultnodesettings.SettingsModelString;
+import org.knime.core.node.port.PortObjectSpec;
+import org.knime.core.node.util.CheckUtils;
+import org.knime.filehandling.core.connections.FSLocationSpec;
+import org.knime.filehandling.core.data.location.FSLocationValueMetaData;
+import org.knime.filehandling.core.data.location.cell.FSLocationCellFactory;
+import org.knime.filehandling.core.defaultnodesettings.filechooser.writer.SettingsModelWriterFileChooser;
+import org.knime.filehandling.core.defaultnodesettings.status.NodeModelStatusConsumer;
+import org.knime.filehandling.core.defaultnodesettings.status.StatusMessage.MessageType;
 
 /**
  * The node model allowing to convert binary objects to files.
@@ -68,9 +83,139 @@ final class BinaryObjectsToFilesNodeModel extends NodeModel {
 
     final BinaryObjectsToFilesNodeConfig m_binaryObjectsToFileNodeConfig;
 
+    private final SettingsModelString m_binaryColumn;
+
+    private final SettingsModelWriterFileChooser m_fileWriterSelectionModel;
+
+    private int m_inputTableIdx;
+
+    private final NodeModelStatusConsumer m_statusConsumer;
+
+    private static final String OUTPUT_LOCATION_COL_NAME = "output location";
+
     BinaryObjectsToFilesNodeModel(final PortsConfiguration config, final BinaryObjectsToFilesNodeConfig nodeSettings) {
         super(config.getInputPorts(), config.getOutputPorts());
         m_binaryObjectsToFileNodeConfig = nodeSettings;
+        m_binaryColumn = m_binaryObjectsToFileNodeConfig.getBinaryObjectsSelectionColumnModel();
+        m_fileWriterSelectionModel = m_binaryObjectsToFileNodeConfig.getFileSettingsModelWriterFileChooser();
+        m_inputTableIdx =
+            config.getInputPortLocation().get(BinaryObjectsToFilesNodeFactory.DATA_TABLE_INPUT_PORT_GRP_NAME)[0];
+        m_statusConsumer = new NodeModelStatusConsumer(EnumSet.of(MessageType.ERROR, MessageType.WARNING));
+    }
+
+    @Override
+    protected PortObjectSpec[] configure(final PortObjectSpec[] inSpecs) throws InvalidSettingsException {
+        final DataTableSpec inputTableSpec = (DataTableSpec)inSpecs[m_inputTableIdx];
+        if (m_binaryColumn.getStringValue() == null) {
+            autoGuess(inSpecs);
+        }
+
+        validate(inSpecs);
+
+        //configure the SettingsModelWriterFileChooser
+        m_fileWriterSelectionModel.configureInModel(inSpecs, m_statusConsumer);
+        m_statusConsumer.setWarningsIfRequired(this::setWarningMessage);
+
+        final BinaryObjectsToFilesCellFactory binaryObjectsToFilesCellFactory =
+            new BinaryObjectsToFilesCellFactory(getNewColumnSpec(inputTableSpec));
+
+        return new DataTableSpec[]{
+            createColumnRearranger(inputTableSpec, binaryObjectsToFilesCellFactory).createSpec()};
+    }
+
+    /**
+     * Automatically select the first column in input table which matches the expected type
+     *
+     * @param inSpecs An array of PortObjectSpec
+     * @throws InvalidSettingsException If no column is found with desired data type
+     */
+    private void autoGuess(final PortObjectSpec[] inSpecs) throws InvalidSettingsException {
+        final DataTableSpec inputTableSpec = (DataTableSpec)inSpecs[m_inputTableIdx];
+        m_binaryColumn.setStringValue(inputTableSpec.stream()//
+            .filter(dcs -> dcs.getType().isCompatible(BinaryObjectDataValue.class))//
+            .map(DataColumnSpec::getName)//
+            .findFirst()//
+            .orElseThrow(() -> new InvalidSettingsException("No applicable column available"))//
+        );
+        setWarningMessage(String.format("Auto-guessed column to convert '%s'", m_binaryColumn.getStringValue()));
+    }
+
+    /**
+     * Validate the selected column in input table
+     *
+     * @param inSpecs An array of PortObjectSpec
+     * @throws InvalidSettingsException If the selected column is not part of the input or doesn't have the correct data
+     *             type
+     */
+    private void validate(final PortObjectSpec[] inSpecs) throws InvalidSettingsException {
+        final DataTableSpec inSpec = (DataTableSpec)inSpecs[m_inputTableIdx];
+        final String pathColName = m_binaryColumn.getStringValue();
+        final int colIndex = inSpec.findColumnIndex(pathColName);
+
+        // check column existence
+        CheckUtils.checkSetting(colIndex >= 0, "The selected column '%s' is not part of the input", pathColName);
+
+        // check column type
+        final DataColumnSpec pathColSpec = inSpec.getColumnSpec(colIndex);
+        if (!pathColSpec.getType().isCompatible(BinaryObjectDataValue.class)) {
+            throw new InvalidSettingsException(
+                String.format("The selected column '%s' has the wrong type", pathColName));
+        }
+
+        if (inSpec.containsName(OUTPUT_LOCATION_COL_NAME)) {
+            setWarningMessage(String.format("The name of the column to create is already taken, using '%s' instead.",
+                getUniqueColumnName(inSpec)));
+        }
+
+    }
+
+    /**
+     * Returns a DataColumnSpec with relevant metadata and column name
+     *
+     * @param inSpec An array of PortObjectSpec
+     * @return A new DataColumnSpec
+     */
+
+    private DataColumnSpec getNewColumnSpec(final DataTableSpec inSpec) {
+
+        final FSLocationSpec location = m_fileWriterSelectionModel.getLocation();
+
+        final FSLocationValueMetaData metaData = new FSLocationValueMetaData(location.getFileSystemCategory(),
+            location.getFileSystemSpecifier().orElse(null));
+
+        final DataColumnSpecCreator fsLocationSpec = new DataColumnSpecCreator(
+            inSpec.containsName(OUTPUT_LOCATION_COL_NAME) ? getUniqueColumnName(inSpec) : OUTPUT_LOCATION_COL_NAME,
+            FSLocationCellFactory.TYPE);
+
+        fsLocationSpec.addMetaData(metaData, true);
+        return fsLocationSpec.createSpec();
+    }
+
+    /**
+     * Returns an instance of ColumnRearranger which appends the new column from BinaryObjectsToFilesCellFactory
+     *
+     * @param inSpec An array of PortObjectSpec
+     * @param factory A object of {@link BinaryObjectsToFilesCellFactory}
+     * @return An instance of ColumnRearranger
+     */
+    private static ColumnRearranger createColumnRearranger(final DataTableSpec inSpec,
+        final BinaryObjectsToFilesCellFactory factory) {
+
+        // now create output spec using a ColumnRearranger
+        final ColumnRearranger colRearranger = new ColumnRearranger(inSpec);
+        colRearranger.append(factory);
+
+        return colRearranger;
+    }
+
+    /**
+     * Return a new unique string based on the input spec, so a new column spec can be created
+     *
+     * @param inputSpec A DataTableSpec object
+     * @return A unique String for creating a new column spec
+     */
+    private static String getUniqueColumnName(final DataTableSpec inputSpec) {
+        return DataTableSpec.getUniqueColumnName(inputSpec, OUTPUT_LOCATION_COL_NAME);
     }
 
     @Override
