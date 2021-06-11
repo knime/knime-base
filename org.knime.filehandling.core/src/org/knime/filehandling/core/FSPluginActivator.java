3ab6047abeb6b8299b19ce96b9e0d290144ab3e8
Bjoern Lohrmann <bjoern.lohrmann@knime.com>
2021-06-11 11:42:06 +0200

    Dummy commit to update build qualifier or filehandling.core (to get rid of m_pathParts error)

--- org.knime.filehandling.core/src/org/knime/filehandling/core/FSPluginActivator.java
+++ org.knime.filehandling.core/src/org/knime/filehandling/core/FSPluginActivator.java
@@ -49,7 +49,6 @@
 package org.knime.filehandling.core;
 
 import org.eclipse.core.runtime.Plugin;
-import org.osgi.framework.BundleContext;
 
 /**
  * Plugin activator of filehandling.core plugin.
@@ -58,37 +57,14 @@ import org.osgi.framework.BundleContext;
  */
 public class FSPluginActivator extends Plugin {
     // The shared instance.
-    private static volatile FSPluginActivator plugin;
+    private static volatile FSPluginActivator plugin; // NOSONAR proven pattern in plugin activators
 
     /**
      * The constructor.
      */
     public FSPluginActivator() {
         super();
-        plugin = this;
-    }
-
-    /**
-     * This method is called upon plug-in activation.
-     *
-     * @param context The OSGI bundle context
-     * @throws Exception If this plugin could not be started
-     */
-    @Override
-    public void start(final BundleContext context) throws Exception {
-        super.start(context);
-    }
-
-    /**
-     * This method is called when the plug-in is stopped.
-     *
-     * @param context The OSGI bundle context
-     * @throws Exception If this plugin could not be stopped
-     */
-    @Override
-    public void stop(final BundleContext context) throws Exception {
-        super.stop(context);
-        plugin = null;
+        plugin = this; // NOSONAR
     }
 
     /**
