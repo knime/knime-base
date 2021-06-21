941bd0c4e2b00a1e50af789834716a59ffdca572
Moditha Hewasinghage <moditha.hewasinghage@knime.com>
2021-06-21 17:48:35 +0000

    AP-16844: BaseFileSystemProvider: Clear cached attributes of the parent folder on write operations
    AP-16844 (SSH Connector: Fix file attribute cache issues after copy/move)

--- org.knime.filehandling.core/src/org/knime/filehandling/core/connections/base/BaseFileSystemProvider.java
+++ org.knime.filehandling.core/src/org/knime/filehandling/core/connections/base/BaseFileSystemProvider.java
@@ -184,6 +184,9 @@ public abstract class BaseFileSystemProvider<P extends FSPath, F extends BaseFil
             checkParentDirectoryExists(checkedPath);
         }
 
+        if (sanitizedOptions.contains(StandardOpenOption.WRITE)) {
+            deleteCachedParentDirectoryAttributes(checkedPath);
+        }
         return new FSSeekableByteChannel(newByteChannelInternal(checkedPath, sanitizedOptions, attrs), m_fileSystem) {
             @Override
             public void close() throws IOException {
@@ -279,6 +282,7 @@ public abstract class BaseFileSystemProvider<P extends FSPath, F extends BaseFil
             throw new IOException(path.toString() + " is a directory");
         }
 
+        deleteCachedParentDirectoryAttributes(checkedPath);
         return new FSInputStream(newInputStreamInternal(checkedPath, options), getFileSystemInternal());
     }
 
@@ -325,6 +329,8 @@ public abstract class BaseFileSystemProvider<P extends FSPath, F extends BaseFil
 
         moveInternal(checkedSource, checkedTarget, options);
         getFileSystemInternal().removeFromAttributeCacheDeep(checkedSource);
+
+        deleteCachedParentDirectoryAttributes(checkedTarget);
     }
 
     /**
@@ -426,6 +432,7 @@ public abstract class BaseFileSystemProvider<P extends FSPath, F extends BaseFil
 
         copyInternal(checkedSource, checkedTarget, options);
         getFileSystemInternal().removeFromAttributeCache(checkedTarget);
+        deleteCachedParentDirectoryAttributes(checkedTarget);
     }
 
     /**
@@ -470,6 +477,7 @@ public abstract class BaseFileSystemProvider<P extends FSPath, F extends BaseFil
         } catch (NoSuchFileException e) { // NOSONAR must be ignored here
         }
 
+        deleteCachedParentDirectoryAttributes(checkedPath);
         return new FSOutputStream(newOutputStreamInternal(checkedPath, validatedOpenOptions), getFileSystemInternal()) {
             @Override
             public void close() throws IOException {
@@ -563,6 +571,7 @@ public abstract class BaseFileSystemProvider<P extends FSPath, F extends BaseFil
             throw new FileAlreadyExistsException(checkedDir.toString());
         } catch (NoSuchFileException e) { // NOSONAR exception is dealt with properly
             createDirectoryInternal(checkedDir, attrs);
+            deleteCachedParentDirectoryAttributes(checkedDir);
         }
     }
 
@@ -629,10 +638,10 @@ public abstract class BaseFileSystemProvider<P extends FSPath, F extends BaseFil
     }
 
     /**
-     * Tests whether the given absolute, normalized path exists in the backing file system. Subclasses can override
-     * this method, if there is a more efficient method than invoking {@link #fetchAttributesInternal(FSPath, Class)}
-     * to determine the existence of the given path. Note that implementations of this method must not perform a
-     * cache lookup (this is already done in {@link #existsCached(FSPath)}.
+     * Tests whether the given absolute, normalized path exists in the backing file system. Subclasses can override this
+     * method, if there is a more efficient method than invoking {@link #fetchAttributesInternal(FSPath, Class)} to
+     * determine the existence of the given path. Note that implementations of this method must not perform a cache
+     * lookup (this is already done in {@link #existsCached(FSPath)}.
      *
      * @param path An absolute and normalized path to check for existence.
      * @return whether the path exists or not.
@@ -780,6 +789,7 @@ public abstract class BaseFileSystemProvider<P extends FSPath, F extends BaseFil
 
         deleteInternal(checkedPath);
         getFileSystemInternal().removeFromAttributeCache(path);
+        deleteCachedParentDirectoryAttributes(checkedPath);
     }
 
     /**
@@ -884,5 +894,11 @@ public abstract class BaseFileSystemProvider<P extends FSPath, F extends BaseFil
     @Override
     public final String getScheme() {
         return getFileSystemInternal().getFileSystemBaseURI().getScheme();
+    }
+
+    private void deleteCachedParentDirectoryAttributes(final P path) {
+        if (path.getParent() != null) {
+            getFileSystemInternal().removeFromAttributeCache(path.getParent());
+        }
     }
 }
