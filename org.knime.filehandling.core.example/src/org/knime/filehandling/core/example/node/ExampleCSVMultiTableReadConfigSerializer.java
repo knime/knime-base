ac877f09b9afbce66811b446a92506c726514581
Moditha Hewasinghage <moditha.hewasinghage@knime.com>
2021-07-01 10:58:17 +0200

    AP-17073: Adding limit rows and path column
    (AP-17073: FH Docs: Write section on TRF specific options)

--- org.knime.filehandling.core.example/src/org/knime/filehandling/core/example/node/ExampleCSVMultiTableReadConfigSerializer.java
+++ org.knime.filehandling.core.example/src/org/knime/filehandling/core/example/node/ExampleCSVMultiTableReadConfigSerializer.java
@@ -52,8 +52,11 @@ import org.knime.core.node.InvalidSettingsException;
 import org.knime.core.node.NodeSettings;
 import org.knime.core.node.NodeSettingsRO;
 import org.knime.core.node.NodeSettingsWO;
+import org.knime.core.node.defaultnodesettings.SettingsModel;
 import org.knime.core.node.port.PortObjectSpec;
 import org.knime.filehandling.core.node.table.reader.config.ConfigSerializer;
+import org.knime.filehandling.core.node.table.reader.config.DefaultTableReadConfig;
+import org.knime.filehandling.core.node.table.reader.config.TableReadConfig;
 import org.knime.filehandling.core.node.table.reader.config.tablespec.ConfigID;
 import org.knime.filehandling.core.node.table.reader.config.tablespec.ConfigIDFactory;
 import org.knime.filehandling.core.node.table.reader.config.tablespec.NodeSettingsConfigID;
@@ -73,41 +76,98 @@ enum ExampleCSVMultiTableReadConfigSerializer
      */
     INSTANCE;
 
+    private static final String KEY = "example_csv_reader";
+
     private static final String CFG_COL_HEADER_PREFIX = "col_header_prefix";
+
+    private static final String CFG_LIMIT_ROWS_TAB = "limit_rows";
+
+    private static final String CFG_LIMIT_DATA_ROWS = "is_limit_rows";
+
+    private static final String CFG_MAX_ROWS = "limit_rows";
+
+    private static final String CFG_SKIP_DATA_ROWS = "is_skip_rows";
+
+    private static final String CFG_NUMBER_OF_ROWS_TO_SKIP = "skip_rows";
+
+    private static final String CFG_APPEND_PATH_COLUMN = "append_path_column" + SettingsModel.CFGKEY_INTERNAL;
+
+    private static final String CFG_PATH_COLUMN_NAME = "path_column_name" + SettingsModel.CFGKEY_INTERNAL;
 
     @Override
     public void loadInDialog(final ExampleCSVMultiTableReadConfig config, final NodeSettingsRO settings,
             final PortObjectSpec[] specs) {
         loadSettingsTabInDialog(config, SettingsUtils.getOrEmpty(settings, SettingsUtils.CFG_SETTINGS_TAB));
+        loadLimitRowsTabInDialog(config, SettingsUtils.getOrEmpty(settings, CFG_LIMIT_ROWS_TAB));
     }
 
-    private static void loadSettingsTabInDialog(final ExampleCSVMultiTableReadConfig config, // NOSONAR
+    private static void loadSettingsTabInDialog(final ExampleCSVMultiTableReadConfig config,
             final NodeSettingsRO settings) {
         final ExampleCSVReaderConfig csvReaderCfg = config.getReaderSpecificConfig();
         csvReaderCfg.setColumnHeaderPrefix(settings.getString(CFG_COL_HEADER_PREFIX, "Column"));
+
+        config.setAppendItemIdentifierColumn(
+                settings.getBoolean(CFG_APPEND_PATH_COLUMN, config.appendItemIdentifierColumn()));
+        config.setItemIdentifierColumnName(
+                settings.getString(CFG_PATH_COLUMN_NAME, config.getItemIdentifierColumnName()));
+
+    }
+
+    private static void loadLimitRowsTabInDialog(final ExampleCSVMultiTableReadConfig config,
+            final NodeSettingsRO settings) {
+        final DefaultTableReadConfig<ExampleCSVReaderConfig> tc = config.getTableReadConfig();
+        tc.setSkipRows(settings.getBoolean(CFG_SKIP_DATA_ROWS, false));
+        tc.setNumRowsToSkip(settings.getLong(CFG_NUMBER_OF_ROWS_TO_SKIP, 0));
+        tc.setLimitRows(settings.getBoolean(CFG_LIMIT_DATA_ROWS, false));
+        tc.setMaxRows(settings.getLong(CFG_MAX_ROWS, 50L));
     }
 
     @Override
     public void loadInModel(final ExampleCSVMultiTableReadConfig config, final NodeSettingsRO settings)
             throws InvalidSettingsException {
         loadSettingsTabInModel(config, settings.getNodeSettings(SettingsUtils.CFG_SETTINGS_TAB));
+        loadLimitRowsTabInModel(config, settings.getNodeSettings(CFG_LIMIT_ROWS_TAB));
     }
 
     private static void loadSettingsTabInModel(final ExampleCSVMultiTableReadConfig config,
-            final NodeSettingsRO settings) {
+            final NodeSettingsRO settings) throws InvalidSettingsException {
         final ExampleCSVReaderConfig csvReaderCfg = config.getReaderSpecificConfig();
         csvReaderCfg.setColumnHeaderPrefix(settings.getString(CFG_COL_HEADER_PREFIX, "Column"));
 
+        config.setAppendItemIdentifierColumn(settings.getBoolean(CFG_APPEND_PATH_COLUMN));
+        config.setItemIdentifierColumnName(settings.getString(CFG_PATH_COLUMN_NAME));
+    }
+
+    private static void loadLimitRowsTabInModel(final ExampleCSVMultiTableReadConfig config,
+            final NodeSettingsRO settings) throws InvalidSettingsException {
+        final DefaultTableReadConfig<ExampleCSVReaderConfig> tc = config.getTableReadConfig();
+        tc.setSkipRows(settings.getBoolean(CFG_SKIP_DATA_ROWS));
+        tc.setNumRowsToSkip(settings.getLong(CFG_NUMBER_OF_ROWS_TO_SKIP));
+        tc.setLimitRows(settings.getBoolean(CFG_LIMIT_DATA_ROWS));
+        tc.setMaxRows(settings.getLong(CFG_MAX_ROWS));
     }
 
     @Override
     public void saveInModel(final ExampleCSVMultiTableReadConfig config, final NodeSettingsWO settings) {
         saveSettingsTab(config, SettingsUtils.getOrAdd(settings, SettingsUtils.CFG_SETTINGS_TAB));
+        saveLimitRowsTab(config, settings.addNodeSettings(CFG_LIMIT_ROWS_TAB));
     }
 
     private static void saveSettingsTab(final ExampleCSVMultiTableReadConfig config, final NodeSettingsWO settings) {
         final ExampleCSVReaderConfig exampleReaderCfg = config.getReaderSpecificConfig();
         settings.addString(CFG_COL_HEADER_PREFIX, exampleReaderCfg.getColumnHeaderPrefix());
+
+        settings.addBoolean(CFG_APPEND_PATH_COLUMN, config.appendItemIdentifierColumn());
+        settings.addString(CFG_PATH_COLUMN_NAME, config.getItemIdentifierColumnName());
+    }
+
+    private static void saveLimitRowsTab(final ExampleCSVMultiTableReadConfig config, final NodeSettingsWO settings) {
+        final TableReadConfig<ExampleCSVReaderConfig> tc = config.getTableReadConfig();
+
+        settings.addBoolean(CFG_SKIP_DATA_ROWS, tc.skipRows());
+        settings.addLong(CFG_NUMBER_OF_ROWS_TO_SKIP, tc.getNumRowsToSkip());
+        settings.addBoolean(CFG_LIMIT_DATA_ROWS, tc.limitRows());
+        settings.addLong(CFG_MAX_ROWS, tc.getMaxRows());
     }
 
     @Override
@@ -119,16 +179,27 @@ enum ExampleCSVMultiTableReadConfigSerializer
     public void validate(final ExampleCSVMultiTableReadConfig config, final NodeSettingsRO settings)
             throws InvalidSettingsException {
         validateSettingsTab(settings.getNodeSettings(SettingsUtils.CFG_SETTINGS_TAB));
+        validateLimitRowsTab(settings.getNodeSettings(CFG_LIMIT_ROWS_TAB));
     }
 
     public static void validateSettingsTab(final NodeSettingsRO settings) throws InvalidSettingsException {
         settings.getString(CFG_COL_HEADER_PREFIX);
+        settings.getBoolean(CFG_APPEND_PATH_COLUMN);
+        settings.getString(CFG_PATH_COLUMN_NAME);
+    }
+
+    private static void validateLimitRowsTab(final NodeSettingsRO settings) throws InvalidSettingsException {
+        settings.getBoolean(CFG_SKIP_DATA_ROWS);
+        settings.getLong(CFG_NUMBER_OF_ROWS_TO_SKIP);
+        settings.getBoolean(CFG_LIMIT_DATA_ROWS);
+        settings.getLong(CFG_MAX_ROWS);
     }
 
     @Override
     public ConfigID createFromConfig(final ExampleCSVMultiTableReadConfig config) {
-        final NodeSettings settings = new NodeSettings("example_csv_reader");
+        final NodeSettings settings = new NodeSettings(KEY);
         saveConfigIDSettingsTab(config, settings.addNodeSettings(SettingsUtils.CFG_SETTINGS_TAB));
+        saveConfigIDLimitRowsTab(config, settings.addNodeSettings(CFG_LIMIT_ROWS_TAB));
         return new NodeSettingsConfigID(settings);
     }
 
@@ -137,8 +208,17 @@ enum ExampleCSVMultiTableReadConfigSerializer
         settings.addString(CFG_COL_HEADER_PREFIX, config.getReaderSpecificConfig().getColumnHeaderPrefix());
     }
 
+    private static void saveConfigIDLimitRowsTab(final ExampleCSVMultiTableReadConfig config,
+            final NodeSettingsWO settings) {
+        final TableReadConfig<ExampleCSVReaderConfig> tc = config.getTableReadConfig();
+        settings.addBoolean(CFG_LIMIT_DATA_ROWS, tc.limitRows());
+        settings.addLong(CFG_MAX_ROWS, tc.getMaxRows());
+        settings.addBoolean(CFG_SKIP_DATA_ROWS, tc.skipRows());
+        settings.addLong(CFG_NUMBER_OF_ROWS_TO_SKIP, tc.getNumRowsToSkip());
+    }
+
     @Override
     public ConfigID createFromSettings(final NodeSettingsRO settings) throws InvalidSettingsException {
-        return new NodeSettingsConfigID(settings.getNodeSettings("example_csv_reader"));
+        return new NodeSettingsConfigID(settings.getNodeSettings(KEY));
     }
 }
