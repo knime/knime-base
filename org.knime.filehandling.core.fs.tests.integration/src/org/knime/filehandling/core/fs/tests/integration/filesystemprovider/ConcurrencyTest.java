39a6eb962f931dc1d9ee044e605f84fe08e373d1
Bjoern Lohrmann <bjoern.lohrmann@knime.com>
2021-08-11 12:19:33 +0200

    AP-17074: Add Eclipse project settings
    AP-17074 (FH Docs: Write section on Writer nodes)

---
+++ org.knime.filehandling.core.fs.tests.integration/src/org/knime/filehandling/core/fs/tests/integration/filesystemprovider/ConcurrencyTest.java
@@ -0,0 +1,97 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright by KNIME AG, Zurich, Switzerland
+ *  Website: http://www.knime.com; Email: contact@knime.com
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME AG herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ *
+ * History
+ *   Jun 15, 2021 (bjoern): created
+ */
+package org.knime.filehandling.core.fs.tests.integration.filesystemprovider;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+import org.knime.filehandling.core.fs.tests.integration.AbstractParameterizedFSTest;
+import org.knime.filehandling.core.testing.FSTestInitializer;
+import org.knime.filehandling.core.util.IOESupplier;
+
+/**
+ *
+ * @author bjoern
+ */
+public class ConcurrencyTest extends AbstractParameterizedFSTest {
+
+    public ConcurrencyTest(final String fsType, final IOESupplier<FSTestInitializer> testInitializer)
+        throws IOException {
+        super(fsType, testInitializer);
+    }
+
+    @Test
+    public void test_concurrent_read() throws IOException, InterruptedException {
+        Path pathToFile = m_testInitializer.createFileWithContent("content", "file.txt");
+
+        ExecutorService pool = Executors.newFixedThreadPool(4);
+        for (int i = 0; i < 30; i++) {
+            Path copyTarget = m_testInitializer.makePath("copy" + i);
+            pool.execute(() -> {
+                try {
+                    Files.copy(pathToFile, copyTarget);
+                } catch (IOException e) {
+                    System.out.printf("%s: %s (%s)", //
+                        Thread.currentThread().getName(), //
+                        e.getMessage(), //
+                        e.getClass().getSimpleName());
+                }
+            });
+        }
+        pool.shutdown();
+        pool.awaitTermination(60, TimeUnit.SECONDS);
+
+    }
+
+}
