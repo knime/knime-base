a4e018376f124f2a06c9d7e00295555624660738
Manuel Hotz <manuel.hotz@knime.com>
2024-09-12 11:42:54 +0200

    AP-23017: Add range specialization for int columns
    AP-23017 (Row Filter to allow equality check between compatible numeric types)

--- org.knime.base/src/org/knime/base/node/preproc/filter/row3/predicates/RangePredicateFactory.java
+++ org.knime.base/src/org/knime/base/node/preproc/filter/row3/predicates/RangePredicateFactory.java
@@ -60,12 +60,20 @@ import org.knime.core.data.DataValueComparatorDelegator;
 import org.knime.core.data.DoubleValue;
 import org.knime.core.data.IntValue;
 import org.knime.core.data.LongValue;
+import org.knime.core.data.def.DoubleCell;
+import org.knime.core.data.def.IntCell;
+import org.knime.core.data.def.LongCell;
 import org.knime.core.data.v2.RowRead;
 import org.knime.core.node.InvalidSettingsException;
 import org.knime.core.node.message.Message;
 import org.knime.core.webui.node.dialog.defaultdialog.widget.dynamic.DynamicValuesInput;
 
-public abstract class RangePredicateFactory extends AbstractPredicateFactory {
+/**
+ * Predicate factory for range predicates (<, <=, >, >=). If possible, the predicate is specialized to primitive values.
+ *
+ * @author Manuel Hotz, KNIME GmbH, Konstanz, Germany
+ */
+abstract class RangePredicateFactory extends AbstractPredicateFactory {
 
     private enum RangeOperator {
             LT, LTE, GT, GTE;
@@ -91,17 +99,18 @@ public abstract class RangePredicateFactory extends AbstractPredicateFactory {
         if (!isSupported) {
             return Optional.empty();
         }
-
+        final PredicateFactory factory;
         final var preferredValueClass = columnDataType.getPreferredValueClass();
         if (preferredValueClass.equals(IntValue.class)) {
-            return Optional.of(new RangeIntPredicateFactory(operator));
+            factory = new RangeIntPredicateFactory(operator);
         } else if (preferredValueClass.equals(LongValue.class)) {
-            return Optional.of(new RangeLongPredicateFactory(operator));
+            factory = new RangeLongPredicateFactory(operator);
         } else if (preferredValueClass.equals(DoubleValue.class)) {
-            return Optional.of(new RangeDoublePredicateFactory(operator));
+            factory = new RangeDoublePredicateFactory(operator);
+        } else {
+            factory = new RangeDataValuePredicateFactory(operator, columnDataType);
         }
-
-        return Optional.of(new RangeDataValuePredicateFactory(operator, columnDataType));
+        return Optional.of(factory);
     }
 
     private static final class RangeIntPredicateFactory extends RangePredicateFactory {
@@ -113,7 +122,55 @@ public abstract class RangePredicateFactory extends AbstractPredicateFactory {
         @Override
         public Predicate<RowRead> createPredicate(final int columnIndex, final DynamicValuesInput inputValues)
             throws InvalidSettingsException {
-            throw new UnsupportedOperationException("Not yet implemented");
+            final var refCell = getCellAtOrThrow(inputValues, 0);
+            if (refCell instanceof IntCell intCell) {
+                // comparing Integer column with int value
+                final var ref = intCell.getIntValue();
+                final var predicate = IntRangePredicate.create(ref, m_operator);
+                return rowRead -> predicate.test(rowRead.<IntValue> getValue(columnIndex).getIntValue());
+            }
+
+            if (refCell instanceof LongCell longCell) {
+                // comparing Integer column with long value
+                final var ref = longCell.getLongValue();
+                return comparingWithLongValue(columnIndex, ref);
+            }
+
+            if (refCell instanceof DoubleCell doubleCell) {
+                // comparing Integer column with double value
+                final var ref = doubleCell.getDoubleValue();
+                return comparingWithDoubleValue(columnIndex, ref);
+            }
+
+            final var refCellType = refCell.getType();
+            throw Message.builder() //
+                .withSummary("Cannot apply range comparison to column of type \"%s\" and reference value of type \"%s\""
+                    .formatted(IntCell.TYPE, refCellType)) //
+                .addResolutions( //
+                    "Reconfigure the node to use a reference value of type \"%s\", \"%s\", or \"%s\"" //
+                        .formatted(IntCell.TYPE, LongCell.TYPE, DoubleCell.TYPE), //
+                    "Convert the input column to \"%s\" before applying the filter, e.g. with an expression node."
+                        .formatted(refCellType)) //
+                .build().orElseThrow().toInvalidSettingsException();
+        }
+
+        private Predicate<RowRead> comparingWithLongValue(final int intColumnIndex, final long ref) {
+            // TODO(performance): take Integer max/min values into account to return static ALWAYS_TRUE/ALWAYS_FALSE
+            //                    and then use int predicate for remaining comparison
+
+            // the column is an IntValue, but the reference is a long, so we need to upcast the column value
+            return rowRead -> LongRangePredicate.create(ref, m_operator)
+                .test(rowRead.<IntValue> getValue(intColumnIndex).getIntValue());
+        }
+
+        private Predicate<RowRead> comparingWithDoubleValue(final int intColumnIndex, final double reference) {
+            // TODO(performance): take Integer max/min values into account to return static ALWAYS_TRUE/ALWAYS_FALSE
+            //                    We might then be able to round the reference depending on the operator
+            //                    and use the int predicate.
+
+            // the column is an IntValue, but the reference is a double, so we need to upcast the column value
+            return rowRead -> DoubleRangePredicate.create(reference, m_operator)
+                .test(rowRead.<IntValue> getValue(intColumnIndex).getIntValue());
         }
 
     }
@@ -161,13 +218,13 @@ public abstract class RangePredicateFactory extends AbstractPredicateFactory {
         @Override
         public Predicate<RowRead> createPredicate(final int columnIndex, final DynamicValuesInput inputValues)
             throws InvalidSettingsException {
-            final var refCell = getCellAtOrThrow(inputValues, 0);
             final var isRowKey = columnIndex < 0;
             if (isRowKey) {
                 throw Message.builder().withSummary("Cannot compare RowIDs by range value")
                     .addResolutions("Convert the RowID into a data column before applying the filter.").build()
                     .orElseThrow().toInvalidSettingsException();
             }
+            final var refCell = getCellAtOrThrow(inputValues, 0);
             final var refType = refCell.getType();
             if (!m_columnType.isASuperTypeOf(refType)) {
                 throw Message.builder()
@@ -187,4 +244,77 @@ public abstract class RangePredicateFactory extends AbstractPredicateFactory {
         }
 
     }
+
+    interface IntRangePredicate {
+        /**
+         * @param value the value to test
+         * @return {@code true} if the value satisfies the range predicate, {@code false} otherwise
+         */
+        boolean test(int value);
+
+        /**
+         * Creates a range predicate for the given bound and operator.
+         *
+         * @param bound the bound to compare against
+         * @param operator the range operator
+         * @return the range predicate
+         */
+        static IntRangePredicate create(final int bound, final RangeOperator operator) {
+            return switch (operator) {
+                case LT -> v -> v < bound;
+                case LTE -> v -> v <= bound;
+                case GT -> v -> v > bound;
+                case GTE -> v -> v >= bound;
+            };
+        }
+    }
+
+    interface LongRangePredicate {
+        /**
+         * @param value the value to test
+         * @return {@code true} if the value satisfies the range predicate, {@code false} otherwise
+         */
+        boolean test(long value);
+
+        /**
+         * Creates a range predicate for the given bound and operator.
+         *
+         * @param bound the bound to compare against
+         * @param operator the range operator
+         * @return the range predicate
+         */
+        static LongRangePredicate create(final long bound, final RangeOperator operator) {
+            return switch (operator) {
+                case LT -> v -> v < bound;
+                case LTE -> v -> v <= bound;
+                case GT -> v -> v > bound;
+                case GTE -> v -> v >= bound;
+            };
+        }
+    }
+
+    interface DoubleRangePredicate {
+        /**
+         * @param value the value to test
+         * @return {@code true} if the value satisfies the range predicate, {@code false} otherwise
+         */
+        boolean test(double value);
+
+        /**
+         * Creates a range predicate for the given bound and operator.
+         *
+         * @param bound the bound to compare against
+         * @param operator the range operator
+         * @return the range predicate
+         */
+        static DoubleRangePredicate create(final double bound, final RangeOperator operator) {
+            return switch (operator) {
+                case LT -> v -> v < bound;
+                case LTE -> v -> v <= bound;
+                case GT -> v -> v > bound;
+                case GTE -> v -> v >= bound;
+            };
+        }
+    }
+
 }
