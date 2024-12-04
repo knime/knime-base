fab056d79c68879b61240497a437804c34b93fc4
Marc Bux <marc.bux@knime.com>
2024-12-04 10:59:13 +0100

    Revert "UIEXT-2324: Add initial triggers within array layout updates where necessary"
    This reverts commit 2ac6523561d6b71207e2403accb9bb14490b0d55.

--- org.knime.base.tests/src/org/knime/base/node/preproc/filter/row3/FilterOperatorTest.java
+++ org.knime.base.tests/src/org/knime/base/node/preproc/filter/row3/FilterOperatorTest.java
@@ -199,11 +199,6 @@ final class FilterOperatorTest {
                 throw new IllegalStateException("Unexpected dependency \"%s\"".formatted(ref.getName()));
             }
 
-            @Override
-            public void computeAfterOpenDialog() {
-                // Do nothing
-            }
-
         });
 
         final var choices = new TypeBasedOperatorChoices();
@@ -219,6 +214,10 @@ final class FilterOperatorTest {
                     "Unexpected provider class \"%s\"".formatted(stateProviderClass.getName()));
             }
 
+            @Override
+            public void computeBeforeOpenDialog() {
+                // expected to be called
+            }
         });
 
         return Arrays.stream(choices.computeState(ctx)).map(idAndText -> FilterOperator.valueOf(idAndText.id()))
--- org.knime.base/src/org/knime/base/node/preproc/filter/row3/AbstractRowFilterNodeSettings.java
+++ org.knime.base/src/org/knime/base/node/preproc/filter/row3/AbstractRowFilterNodeSettings.java
@@ -520,7 +520,6 @@ abstract class AbstractRowFilterNodeSettings implements DefaultNodeSettings {
 
         @Override
         public void init(final StateProviderInitializer initializer) {
-            initializer.computeAfterOpenDialog();
             m_columnSelection = initializer.computeFromValueSupplier(SelectedColumnRef.class);
         }
 
