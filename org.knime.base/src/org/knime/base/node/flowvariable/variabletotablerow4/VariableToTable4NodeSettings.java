d9c174d67dc0d96969b17187ef899e61e2200428
Martin Sillye <martin.sillye@tngtech.com>
2025-01-30 12:55:50 +0100

    UIEXT-1656: Add WebUI for Variable to Table Row node
    UIEXT-1656 (Web UI for Variable to Table Row)

---
+++ org.knime.base/src/org/knime/base/node/flowvariable/variabletotablerow4/VariableToTable4NodeSettings.java
@@ -0,0 +1,89 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright by KNIME AG, Zurich, Switzerland
+ *  Website: http://www.knime.com; Email: contact@knime.com
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME AG herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ *
+ * History
+ *   Jan 29, 2025 (Martin Sillye, TNG Technology Consulting GmbH): created
+ */
+package org.knime.base.node.flowvariable.variabletotablerow4;
+
+import org.knime.base.node.flowvariable.converter.variabletocell.VariableToCellConverterFactory;
+import org.knime.base.node.flowvariable.converter.variabletocell.VariableToDataColumnConverter;
+import org.knime.core.data.DataColumnSpec;
+import org.knime.core.webui.node.dialog.defaultdialog.DefaultNodeSettings;
+import org.knime.core.webui.node.dialog.defaultdialog.setting.columnfilter.ColumnFilter;
+import org.knime.core.webui.node.dialog.defaultdialog.widget.ChoicesWidget;
+import org.knime.core.webui.node.dialog.defaultdialog.widget.ColumnChoicesStateProvider;
+import org.knime.core.webui.node.dialog.defaultdialog.widget.Widget;
+
+/**
+ * The settings for the "Variable to Table Row" node.
+ *
+ * @author Martin Sillye, TNG Technology Consulting GmbH
+ */
+@SuppressWarnings("restriction")
+final class VariableToTable4NodeSettings implements DefaultNodeSettings {
+
+    @Widget(title = "Row name", description = "The name of the RowID used for the resulting row.")
+    String m_rowName = "Row0";
+
+    @Widget(title = "Variable filter", description = """
+            Include list contains all variables that are converted into a column of the resulting row, excluded \
+            variables are not used. All variables remain on the variable stack.
+            """)
+    @ChoicesWidget(choicesProvider = VariableColumnsProvider.class)
+    ColumnFilter m_filter = new ColumnFilter();
+
+    static final class VariableColumnsProvider implements ColumnChoicesStateProvider {
+
+        @Override
+        public DataColumnSpec[] columnChoices(final DefaultNodeSettingsContext context) {
+            try (final var converter = new VariableToDataColumnConverter()) {
+                return context.getAvailableInputFlowVariables(VariableToCellConverterFactory.getSupportedTypes())
+                    .values().stream().map(variable -> converter.createSpec(variable.getName(), variable))
+                    .toArray(DataColumnSpec[]::new);
+            }
+        }
+    }
+}
