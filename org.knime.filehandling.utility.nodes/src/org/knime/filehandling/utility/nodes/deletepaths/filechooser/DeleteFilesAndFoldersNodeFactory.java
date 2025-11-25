84559c9cb37ef17ccff4eb925d9c644da3ed8e17
Thomas Reifenberger <thomas.reifenberger@tngtech.com>
2025-11-25 14:50:57 +0100

    UIEXT-3077: Migrate node
    UIEXT-3077 (WebUI-Migration Delete Files/Folders)

--- org.knime.filehandling.utility.nodes/src/org/knime/filehandling/utility/nodes/deletepaths/filechooser/DeleteFilesAndFoldersNodeFactory.java
+++ org.knime.filehandling.utility.nodes/src/org/knime/filehandling/utility/nodes/deletepaths/filechooser/DeleteFilesAndFoldersNodeFactory.java
@@ -47,39 +47,56 @@
  */
 package org.knime.filehandling.utility.nodes.deletepaths.filechooser;
 
+import static org.knime.node.impl.description.PortDescription.dynamicPort;
+import static org.knime.node.impl.description.PortDescription.fixedPort;
+
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.knime.core.node.BufferedDataTable;
 import org.knime.core.node.ConfigurableNodeFactory;
+import org.knime.core.node.NodeDescription;
 import org.knime.core.node.NodeDialogPane;
 import org.knime.core.node.NodeView;
 import org.knime.core.node.context.NodeCreationConfiguration;
+import org.knime.core.webui.node.dialog.NodeDialog;
+import org.knime.core.webui.node.dialog.NodeDialogFactory;
+import org.knime.core.webui.node.dialog.NodeDialogManager;
+import org.knime.core.webui.node.dialog.SettingsType;
+import org.knime.core.webui.node.dialog.defaultdialog.DefaultKaiNodeInterface;
+import org.knime.core.webui.node.dialog.defaultdialog.DefaultNodeDialog;
+import org.knime.core.webui.node.dialog.kai.KaiNodeInterface;
+import org.knime.core.webui.node.dialog.kai.KaiNodeInterfaceFactory;
 import org.knime.filehandling.core.port.FileSystemPortObject;
 import org.knime.filehandling.utility.nodes.deletepaths.AbstractDeleteFilesAndFoldersNodeConfig;
+import org.knime.node.impl.description.DefaultNodeDescriptionUtil;
+import org.knime.node.impl.description.PortDescription;
 
 /**
  * Node Factory of the "Delete Files/Folders" node.
  *
  * @author Timmo Waller-Ehrat, KNIME GmbH, Konstanz, Germany
+ * @author Thomas Reifenberger, TNG Technology Consulting GmbH
+ * @author AI Migration Pipeline v1.2
  */
-public final class DeleteFilesAndFoldersNodeFactory extends ConfigurableNodeFactory<DeleteFilesAndFoldersNodeModel> {
+@SuppressWarnings("restriction")
+public final class DeleteFilesAndFoldersNodeFactory extends ConfigurableNodeFactory<DeleteFilesAndFoldersNodeModel>
+    implements NodeDialogFactory, KaiNodeInterfaceFactory {
 
     @Override
     protected Optional<PortsConfigurationBuilder> createPortsConfigBuilder() {
         PortsConfigurationBuilder b = new PortsConfigurationBuilder();
-        b.addOptionalInputPortGroup(AbstractDeleteFilesAndFoldersNodeConfig.CONNECTION_INPUT_PORT_GRP_NAME, FileSystemPortObject.TYPE);
+        b.addOptionalInputPortGroup(AbstractDeleteFilesAndFoldersNodeConfig.CONNECTION_INPUT_PORT_GRP_NAME,
+            FileSystemPortObject.TYPE);
         b.addFixedOutputPortGroup(AbstractDeleteFilesAndFoldersNodeConfig.OUTPUT_PORT_GRP_NAME, BufferedDataTable.TYPE);
         return Optional.of(b);
     }
 
     @Override
     protected DeleteFilesAndFoldersNodeModel createNodeModel(final NodeCreationConfiguration creationConfig) {
-        return new DeleteFilesAndFoldersNodeModel(creationConfig.getPortConfig().orElseThrow(IllegalStateException::new));
-    }
-
-    @Override
-    protected NodeDialogPane createNodeDialogPane(final NodeCreationConfiguration creationConfig) {
-        return new DeleteFilesAndFoldersNodeDialog(creationConfig.getPortConfig().orElseThrow(IllegalStateException::new));
+        return new DeleteFilesAndFoldersNodeModel(
+            creationConfig.getPortConfig().orElseThrow(IllegalStateException::new));
     }
 
     @Override
@@ -88,12 +105,64 @@ public final class DeleteFilesAndFoldersNodeFactory extends ConfigurableNodeFact
     }
 
     @Override
-    public NodeView<DeleteFilesAndFoldersNodeModel> createNodeView(final int viewIndex, final DeleteFilesAndFoldersNodeModel nodeModel) {
+    public NodeView<DeleteFilesAndFoldersNodeModel> createNodeView(final int viewIndex,
+        final DeleteFilesAndFoldersNodeModel nodeModel) {
         return null;
     }
 
     @Override
     public boolean hasDialog() {
         return true;
+    }
+
+    private static final String NODE_NAME = "Delete Files/Folders";
+
+    private static final String NODE_ICON = "../delete16x16.png";
+
+    private static final String SHORT_DESCRIPTION = """
+            Deletes selected files as well as folders.
+            """;
+
+    private static final String FULL_DESCRIPTION =
+        """
+                <p> This node deletes the files or folders at a specified location. The location may be a remote
+                    location. Folders will also be deleted if they still contain files. </p> <p> <i>This node can access
+                    a variety of different</i> <a
+                    href="https://docs.knime.com/2021-06/analytics_platform_file_handling_guide/index.html#analytics-platform-file-systems"><i>file
+                    systems.</i></a> <i>More information about file handling in KNIME can be found in the official</i>
+                    <a href="https://docs.knime.com/latest/analytics_platform_file_handling_guide/index.html"><i>File
+                    Handling Guide.</i></a> </p>
+                """;
+
+    private static final List<PortDescription> INPUT_PORTS =
+        List.of(dynamicPort("File System Connection", "File System Connection", """
+                The file system connection.
+                """));
+
+    private static final List<PortDescription> OUTPUT_PORTS = List.of(fixedPort("Output table", """
+            Table containing the list of files that have been deleted. If deletion is allowed to fail, there is an
+            additional column, that will indicate if the deletion was successful or not.
+            """));
+
+    @Override
+    public NodeDialogPane createNodeDialogPane(final NodeCreationConfiguration creationConfig) {
+        return NodeDialogManager.createLegacyFlowVariableNodeDialog(createNodeDialog());
+    }
+
+    @Override
+    public NodeDialog createNodeDialog() {
+        return new DefaultNodeDialog(SettingsType.MODEL, DeleteFilesAndFoldersNodeParameters.class);
+    }
+
+    @Override
+    public NodeDescription createNodeDescription() {
+        return DefaultNodeDescriptionUtil.createNodeDescription(NODE_NAME, NODE_ICON, INPUT_PORTS, OUTPUT_PORTS,
+            SHORT_DESCRIPTION, FULL_DESCRIPTION, List.of(), DeleteFilesAndFoldersNodeParameters.class, null,
+            NodeType.Other, List.of(), null);
+    }
+
+    @Override
+    public KaiNodeInterface createKaiNodeInterface() {
+        return new DefaultKaiNodeInterface(Map.of(SettingsType.MODEL, DeleteFilesAndFoldersNodeParameters.class));
     }
 }
