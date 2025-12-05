1f1f8e83245d2e7d0fb41e4e860fcb6aa2c4afc0
Marc Bux <marc.bux@knime.com>
2025-12-05 12:27:14 +0100

    UIEXT-3105: Fix order of dynamic / fixed input ports
    - this has been wrongly generated by the AI migration pipeline (see fix
    for the AI migration pipeline)
    - these two nodes seem to be the only affected ones
    
    UIEXT-3105 (WebUI-Migration Table Writer)

--- org.knime.filehandling.utility.nodes/src/org/knime/filehandling/utility/nodes/tempdir/CreateTempDir2NodeFactory.java
+++ org.knime.filehandling.utility.nodes/src/org/knime/filehandling/utility/nodes/tempdir/CreateTempDir2NodeFactory.java
@@ -113,11 +113,12 @@ public final class CreateTempDir2NodeFactory extends ConfigurableNodeFactory<Cre
                     Guide.</i></a> </p>
                 """;
 
-    private static final List<PortDescription> INPUT_PORTS = List.of(fixedPort("Input variables (optional)", """
-            Input variables (optional).
-            """), dynamicPort("File System Connection", "File system connection", """
-            The file system connection.
-            """));
+    private static final List<PortDescription> INPUT_PORTS =
+        List.of(dynamicPort(CONNECTION_INPUT_PORT_GRP_NAME, "File system connection", """
+                The file system connection.
+                """), fixedPort("Input variables (optional)", """
+                Input variables (optional).
+                """));
 
     private static final List<PortDescription> OUTPUT_PORTS =
         List.of(fixedPort("Flow Variables with path information", """
