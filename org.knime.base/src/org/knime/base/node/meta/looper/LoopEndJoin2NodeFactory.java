072a64ec106b6b3603e9e052bfced7dca7799066
Magnus Gohm <magnus.gohm@knime.com>
2026-01-27 11:08:44 +0100

    UIEXT-3258: WebUI Migration of Loop End (Column Append)
    UIEXT-3258 (WebUI Migration Loop End (Column Append))

--- org.knime.base/src/org/knime/base/node/meta/looper/LoopEndJoin2NodeFactory.java
+++ org.knime.base/src/org/knime/base/node/meta/looper/LoopEndJoin2NodeFactory.java
@@ -47,56 +47,142 @@
  */
 package org.knime.base.node.meta.looper;
 
+import static org.knime.node.impl.description.PortDescription.fixedPort;
+
+import java.util.List;
+import java.util.Map;
+
+import org.knime.core.node.NodeDescription;
 import org.knime.core.node.NodeDialogPane;
 import org.knime.core.node.NodeFactory;
 import org.knime.core.node.NodeView;
+import org.knime.core.webui.node.dialog.NodeDialog;
+import org.knime.core.webui.node.dialog.NodeDialogFactory;
+import org.knime.core.webui.node.dialog.NodeDialogManager;
+import org.knime.core.webui.node.dialog.SettingsType;
+import org.knime.core.webui.node.dialog.defaultdialog.DefaultKaiNodeInterface;
+import org.knime.core.webui.node.dialog.defaultdialog.DefaultNodeDialog;
+import org.knime.core.webui.node.dialog.kai.KaiNodeInterface;
+import org.knime.core.webui.node.dialog.kai.KaiNodeInterfaceFactory;
+import org.knime.node.impl.description.DefaultNodeDescriptionUtil;
+import org.knime.node.impl.description.ExternalResource;
+import org.knime.node.impl.description.PortDescription;
 
 /**
- * Loop End Node that joins the input table with the previous input
- * (colum wise concatenation).
+ * Loop End Node that joins the input table with the previous input (colum wise concatenation).
  *
  * @author Bernd Wiswedel, KNIME AG, Zurich, Switzerland
+ * @author Magnus Gohm, KNIME GmbH, Konstanz, Germany
+ * @author AI Migration Pipeline v1.2
  * @since 2.12
  */
-public final class LoopEndJoin2NodeFactory extends NodeFactory<LoopEndJoinNodeModel> {
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    protected NodeDialogPane createNodeDialogPane() {
-        return new LoopEndJoinNodeDialogPane();
-    }
+@SuppressWarnings("restriction")
+public final class LoopEndJoin2NodeFactory extends NodeFactory<LoopEndJoinNodeModel>
+    implements NodeDialogFactory, KaiNodeInterfaceFactory {
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
     public LoopEndJoinNodeModel createNodeModel() {
         return new LoopEndJoinNodeModel(false);
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
     protected int getNrNodeViews() {
         return 0;
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
     protected boolean hasDialog() {
         return true;
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
     public NodeView<LoopEndJoinNodeModel> createNodeView(final int index,
             final LoopEndJoinNodeModel model) {
         return null;
     }
+
+    private static final String NODE_NAME = "Loop End (Column Append)";
+
+    private static final String NODE_ICON = "loop_end_column_append.png";
+
+    private static final String SHORT_DESCRIPTION = """
+            Node at the end of a loop, collecting the intermediate results by joining the tables on their RowIDs.
+            """;
+
+    private static final String FULL_DESCRIPTION = """
+            <p> Node at the end of a loop, collecting the intermediate results by joining the tables on their
+                RowIDs. In each iteration the node will join the current input table with the previous result. The join
+                is based on the RowID on the incoming tables. </p> <p> The typical use case of this node is that you
+                calculate a set of new columns in the loop body and then feed only the newly created columns to this
+                loop end node. It will join all intermediate results (the results from each iteration) using a join
+                operation (full outer join on the RowID column). It is strongly recommended that you filter the original
+                input data from the input as it will otherwise occur multiple times in the joined output. Use a Joiner
+                node following this loop end node in order to join the result with the original input data (the data
+                provided to the loop start node). </p>
+            """;
+
+    private static final List<PortDescription> INPUT_PORTS = List.of(
+            fixedPort("Any datatable", """
+                Any datatable.
+                """)
+    );
+
+    private static final List<PortDescription> OUTPUT_PORTS = List.of(
+            fixedPort("Collected results", """
+                Collected results from the loop body.
+                """)
+    );
+
+    private static final List<ExternalResource> LINKS = List.of(
+         new ExternalResource(
+            "https://docs.knime.com/latest/analytics_platform_flow_control_guide/index.html#loops", """
+                KNIME Flow Control Guide: Section Loops
+                """)
+    );
+
+    /**
+     * {@inheritDoc}
+     * @since 5.10
+     */
+    @Override
+    public NodeDialogPane createNodeDialogPane() {
+        return NodeDialogManager.createLegacyFlowVariableNodeDialog(createNodeDialog());
+    }
+
+    /**
+     * {@inheritDoc}
+     * @since 5.10
+     */
+    @Override
+    public NodeDialog createNodeDialog() {
+        return new DefaultNodeDialog(SettingsType.MODEL, LoopEndJoin2NodeParameters.class);
+    }
+
+    @Override
+    public NodeDescription createNodeDescription() {
+        return DefaultNodeDescriptionUtil.createNodeDescription( //
+            NODE_NAME, //
+            NODE_ICON, //
+            INPUT_PORTS, //
+            OUTPUT_PORTS, //
+            SHORT_DESCRIPTION, //
+            FULL_DESCRIPTION, //
+            LINKS, //
+            LoopEndJoin2NodeParameters.class, //
+            null, //
+            NodeType.LoopEnd, //
+            List.of(), //
+            null //
+        );
+    }
+
+    /**
+     * {@inheritDoc}
+     * @since 5.10
+     */
+    @Override
+    public KaiNodeInterface createKaiNodeInterface() {
+        return new DefaultKaiNodeInterface(Map.of(SettingsType.MODEL, LoopEndJoin2NodeParameters.class));
+    }
+
 }
